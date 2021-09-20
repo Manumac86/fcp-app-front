d6476cfafa648408e959fd1672c9181bd8bfe70e
= <=>
2021-09-20 02:10:52 -0300

    Header styles and logos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Logo from './Logo.jsx';
 import searchIcon from '../../assets/searchIcon.svg';
 import logoVamosLasBandas from '../../assets/vamosLasBandasLogo.svg';
+import logoFCP from '../../assets/FCPLogoBlackLetters.png';
 
 const Header = ({ searchText, onSearch }) => {
   return (
     <div className="Header">
       <div className="Header_Logo_Container">
         <Logo img={logoVamosLasBandas} styleClassName={'fcp'} />
+        <Logo img={logoFCP} styleClassName={'fcp'} />
       </div>
       <div className="container Header_Search_Container">
         <div className="input-group Header_Search">
