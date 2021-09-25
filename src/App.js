bf82d34950476a66635f2fa52f4a916282f32e37
= <=>
2021-09-25 18:14:12 -0300

    Fix loading page

--- src/App.js
+++ src/App.js
@@ -178,7 +178,7 @@ function App() {
         />
       }
       {
-        !isContestActive && <div className="VideoList" style={{height: 'calc(100vh - 80px', fontSize: "3em"}}>No hay concursos activos! Verifica nuestras redes para saber más.</div>
+        !isContestActive && !isLoadingVideos && <div className="VideoList" style={{height: 'calc(100vh - 80px', fontSize: "3em"}}>No hay concursos activos! Verifica nuestras redes para saber más.</div>
       }
       <Footer />
       {isModalOpen && (
